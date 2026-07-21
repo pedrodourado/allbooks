66bb7cdeda808953a3790a439c651b0140eb4091
Pedro Dourado <pedrodourado@gmail.com>
2026-07-21 17:18:04 +0100

    alterar o server.js

--- src/server.js
+++ src/server.js
@@ -18,6 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("Boas vindas ao servidor da API de livros!");
-  console.log("API disponível através da http://localhost:8000");
+  console.log("API disponível em http://localhost:8000")
 })
